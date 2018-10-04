0e4523c4a5949d83928a902eae22cb259b2df288
Ryo Ota <nwtgck@gmail.com>
2018-10-05 01:07:13 +0900

    [AUTO] Update document

--- js/app.js
+++ js/app.js
@@ -19,49 +19,91 @@ function b64DecodeUnicode(str) {
   }).join(''));
 }
 
+var RubyTranspiler = {
+  getExecutableFunction: function(rubyScript){
+    // Use javascript global variable "INPUT"
+    // (NOTE: `INPUT` will be pure JavaScript string variable)
+    var rubyScriptWithInput = 's = `window.INPUT`\n' + rubyScript;
+    // Transpile Ruby to JavaScript
+    var transpiledJsCode = Opal.compile(rubyScriptWithInput);
+    // Remove the first comment from transpiled code
+    transpiledJsCode = transpiledJsCode.replace(/\/\*.*\*\/\s*/, '');
+    // Set executable function
+    var executableFunction = new Function("return " + transpiledJsCode);
+    return executableFunction;
+  }
+}
+
+var Es2017Transpiler = {
+  getExecutableFunction: function(script){
+    // Use javascript global variable "INPUT" 
+    // (NOTE: `INPUT` will be pure JavaScript string variable)    
+    var scriptWithInput = 'var s = window.INPUT;\n' + script;
+    // Transpile
+    var code = Babel.transform(scriptWithInput, {presets: ["es2017"]}).code;
+    return function(){
+      return eval(code);
+    };
+  }
+}
+
 angular.module("nipp", [])
-  // NOTE: Don't use $location because it escapes "/"
+  // NOTE: Don't use $location.hash() because it escapes "/"
   .controller('mainCtrl', ['$scope', function($scope){
     // Set empty string as default input
     $scope.inputText  = "";
-    // Set decoded location.hash as default Ruby script
-    $scope.rubyScript = b64DecodeUnicode(location.hash.substring(1));
-    // Executable function which return Ruby result
+    // Set decoded location.hash as default script
+    $scope.script = b64DecodeUnicode(location.hash.substring(1));
+    // Executable function which return result
     var executableFunction = function(){return "";};
+    // Set default output
+    setOutputText();
+    // Set transpiler
+    var transpiler;
+    switch (location.search) {
+      case "?es2017":
+        console.log("Mode: ES2017")
+        transpiler = Es2017Transpiler;
+        break;
+      default:
+        console.log("Mode: Opal");
+        transpiler = RubyTranspiler;
+    }
     // Set default value to global variable "INPUT"
     window.INPUT = $scope.inputText;
 
-    // Watch Ruby script changes
+    $scope.$watch('inputText', function(){
+      // Set output text
+      setOutputText();
+    });
+
+    // Watch script changes
     // (from: https://stackoverflow.com/a/15424144/2885946)
-    $scope.$watch('rubyScript', function(){
-      // Convert Ruby script to Base64
-      var base64RubyScript = b64EncodeUnicode($scope.rubyScript);
+    $scope.$watch('script', function(){
+      // Convert script to Base64
+      var base64Script = b64EncodeUnicode($scope.script);
       // Change location hash to the code
-      location.hash = base64RubyScript;
+      location.hash = base64Script;
 
       try {
-        // Use javascript global variable "INPUT"
-        // (NOTE: `INPUT` will be pure JavaScript string variable)
-        var rubyScriptWithInput = 's = `window.INPUT`\n' + $scope.rubyScript;
-        // Transpile Ruby to JavaScript
-        var transpiledJsCode = Opal.compile(rubyScriptWithInput);
-        // Remove the first comment from transpiled code
-        transpiledJsCode = transpiledJsCode.replace(/\/\*.*\*\/\s*/, '');
-        // Set executable function
-        executableFunction = new Function("return " + transpiledJsCode);
+        // Transpile script and Set executable function
+        executableFunction = transpiler.getExecutableFunction($scope.script);
+        // Set output text
+        setOutputText();
       } catch (err) {
-        console.log("Ruby compile", err);
+        console.log("Transpile compile", err);
       }
     }, true);
 
-    $scope.outputText = function(){
+    function setOutputText(){
       // Set global INPUT string variable
       window.INPUT = $scope.inputText;
       try {
-        var rubyOutput = executableFunction();
+        var output = executableFunction();
       } catch (err) {
-        console.log("JS error", err)
+        console.log("JS Runtime error", err)
       }
-      return rubyOutput;
+      // Set output text
+      $scope.outputText = output;
     };
   }]);
