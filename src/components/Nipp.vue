b4016a2e59c4ead2346111af2f7b1af33f4e8c6c
Ryo Ota <nwtgck@gmail.com>
2019-04-06 09:33:14 +0900

    [refactor] Clean code up

--- src/components/Nipp.vue
+++ src/components/Nipp.vue
@@ -1,62 +1,61 @@
 <template>
-  <!-- TODO: Remove angular attributes start with "ng-" -->
   <div>
-      <form class="pure-form pure-g">
-        <input type="text" v-model="pageTitle" placeholder="App name" class="pure-u-11-12">
-        <div class="pure-u-1-12" style="text-align: center">
-          <img src="../assets/twitter.png" alt="Share on Twitter" v-on:click="shareOnTwitter()" style="width: 2em; height: 2em;">
-        </div>
-      </form>
+    <form class="pure-form pure-g">
+      <input type="text" v-model="pageTitle" placeholder="App name" class="pure-u-11-12">
+      <div class="pure-u-1-12" style="text-align: center">
+        <img src="../assets/twitter.png" alt="Share on Twitter" v-on:click="shareOnTwitter()" style="width: 2em; height: 2em;">
+      </div>
+    </form>
 
+    <div class="pure-g">
+      <div class="pure-u-1">
+        <!-- TODO: Add proper Editor -->
+        <!-- <div ace-editor="{onLoad: onLoadScriptEditor, mode: transpiler.aceEditorMode, advanced: {tabSize: 2}}" ng-hide="useTextarea" style="height:10em" ng-model="script"></div> -->
+        <textarea v-model="script" style="height:10em; width: 100%" placeholder="Code"></textarea>
+      </div>
+    </div>
+    <form class="pure-form pure-form-aligned">
+      <label for="transpiler">Transpiler:</label>
+      <select id="transpiler" v-model="transpiler" v-on:change="onChangeTranspiler()">
+        <option v-for="t in transpilers" v-bind:value="t" >{{ t.name }}</option>
+      </select>
+
+      <label for="compression_alg">Compression:</label>
+      <select id="compression_alg" v-model="compressionAlg" >
+        <option v-for="a in compressionAlgs" v-bind:value="a" >{{ a.name }}</option>
+      </select>
+
+      <input type="checkbox" v-model="enableClickRun" v-on:change="setLocationHash()">: click_run
+      <input type="checkbox" v-model="enablePromiseWait" v-on:change="setLocationHash()">: promise_wait
+
+      <button v-if="enableClickRun" v-on:click="onClickClickRun()" class="pure-button" style="color: white; background: rgb(28, 184, 65)">
+        {{ clickRunButtonText }}
+      </button>
+    </form>
+
+    <form class="pure-form pure-g">
+      <textarea v-model="inputText" placeholder="Input" rows="10" class="pure-u-1 pure-u-md-1-2"></textarea>
+      <textarea v-model="outputText" placeholder="Output" rows="10" class="pure-u-1 pure-u-md-1-2"></textarea>
+    </form>
+
+    <button v-on:click="showError = !showError" v-bind:style="{color: hasError? 'red': ''}" class="pure-button">Show/Hide error</button>
+    <span v-if="showError">
+      <div class="pure-g">
+        <textarea v-model="errorStr" class="pure-u-1"></textarea>
+      </div>
+    </span>
+
+    <button v-on:click="showTranspiledJsCode = !showTranspiledJsCode" class="pure-button">Show/Hide transpiled JS code</button>
+    <span v-if="showTranspiledJsCode">
       <div class="pure-g">
         <div class="pure-u-1">
           <!-- TODO: Add proper Editor -->
-          <!-- <div ace-editor="{onLoad: onLoadScriptEditor, mode: transpiler.aceEditorMode, advanced: {tabSize: 2}}" ng-hide="useTextarea" style="height:10em" ng-model="script"></div> -->
-          <textarea v-model="script" style="height:10em; width: 100%" placeholder="Code"></textarea>
+          <!-- <div ace-editor="{mode: 'javascript'}" ng-if="!useTextarea" style="height:10em" ng-model="transpiledJsCode"></div> -->
+          <textarea v-model="transpiledJsCode" style="height:10em; width: 100%"></textarea>
         </div>
       </div>
-      <form class="pure-form pure-form-aligned">
-        <label for="transpiler">Transpiler:</label>
-        <select id="transpiler" v-model="transpiler" v-on:change="onChangeTranspiler()">
-          <option v-for="t in transpilers" v-bind:value="t" >{{ t.name }}</option>
-        </select>
-
-        <label for="compression_alg">Compression:</label>
-          <select id="compression_alg" v-model="compressionAlg" >
-            <option v-for="a in compressionAlgs" v-bind:value="a" >{{ a.name }}</option>
-          </select>
-
-        <input type="checkbox" v-model="enableClickRun" v-on:change="setLocationHash()">: click_run
-        <input type="checkbox" v-model="enablePromiseWait" v-on:change="setLocationHash()">: promise_wait
-
-        <button v-if="enableClickRun" v-on:click="onClickClickRun()" class="pure-button" style="color: white; background: rgb(28, 184, 65)">
-          {{ clickRunButtonText }}
-        </button>
-      </form>
-
-      <form class="pure-form pure-g">
-        <textarea v-model="inputText" placeholder="Input" rows="10" class="pure-u-1 pure-u-md-1-2"></textarea>
-        <textarea v-model="outputText" placeholder="Output" rows="10" class="pure-u-1 pure-u-md-1-2"></textarea>
-      </form>
-
-      <button v-on:click="showError = !showError" v-bind:style="{color: hasError? 'red': ''}" class="pure-button">Show/Hide error</button>
-      <span v-if="showError">
-        <div class="pure-g">
-          <textarea v-model="errorStr" class="pure-u-1"></textarea>
-        </div>
-      </span>
-
-      <button v-on:click="showTranspiledJsCode = !showTranspiledJsCode" class="pure-button">Show/Hide transpiled JS code</button>
-      <span v-if="showTranspiledJsCode">
-        <div class="pure-g">
-          <div class="pure-u-1">
-            <!-- TODO: Add proper Editor -->
-            <!-- <div ace-editor="{mode: 'javascript'}" ng-if="!useTextarea" style="height:10em" ng-model="transpiledJsCode"></div> -->
-            <textarea v-model="transpiledJsCode" style="height:10em; width: 100%"></textarea>
-          </div>
-        </div>
-      </span>
-    </div>
+    </span>
+  </div>
 </template>
 
 <script lang="ts">
@@ -82,7 +81,7 @@ const DeflateAlg: CompressionAlg = {
   compress: (str: string) => {
     // NOTE: Negative windowBits means no header and no checksum
     // (see: https://docs.python.org/3.6/library/zlib.html#zlib.decompress)
-    var binStr = pako.deflate(str, {to: 'string', level: 9, windowBits: -8});
+    const binStr = pako.deflate(str, {to: 'string', level: 9, windowBits: -8});
     return binStr;
   },
   decompress: (binStr: string) => {
@@ -124,13 +123,14 @@ const RubyTranspiler: Transpiler = {
   getExecutableFunctionAndTranspiledJsCode: (rubyScript: string) => {
     // Use javascript global variable "INPUT"
     // (NOTE: `INPUT` will be pure JavaScript string variable)
-    var rubyScriptWithInput = 's = `window.INPUT`\n' + rubyScript;
+    const rubyScriptWithInput = 's = `window.INPUT`\n' + rubyScript;
     // Transpile Ruby to JavaScript
-    var transpiledJsCode = Opal.compile(rubyScriptWithInput);
+    const transpiledJsCode =
+      Opal.compile(rubyScriptWithInput)
     // Remove the first comment from transpiled code
-    transpiledJsCode = transpiledJsCode.replace(/\/\*.*\*\/\s*/, '');
+          .replace(/\/\*.*\*\/\s*/, '');
     // Set executable function
-    var executableFunction = new Function("return " + transpiledJsCode);
+    const executableFunction = new Function("return " + transpiledJsCode);
     return {
       executableFunction: executableFunction,
       transpiledJsCode: transpiledJsCode
@@ -191,7 +191,7 @@ function encodeCode(code: string, compressor: (raw: string) => string) {
 function decodeCode(encodedCode: string, decompressor: (compressed: string) => string) {
   try {
     // Base64 => binary String
-    var binStr = atob(encodedCode);
+    const binStr = atob(encodedCode);
     return decompressor(binStr);
   } catch (err) {
     return "";
@@ -244,7 +244,7 @@ export default class Nipp extends Vue {
   // Whether transpiled JS code is shown or not
   showTranspiledJsCode = false;
   // Executable function which return result
-  executableFunction: Function = function(){return "";};
+  executableFunction: Function = () => {return "";};
   // Enable click-run or not
   // (click-run: Non-realtime/non-reactive evaluation)
   enableClickRun = false;
@@ -345,7 +345,7 @@ export default class Nipp extends Vue {
     const encodedCode = encodeCode(this.script, this.compressionAlg.compress);
     // Change location hash to the code
     location.hash = titlePart+"/"+urlOptionsPart+"/"+encodedCode;
-  };
+  }
 
   // Generate options part
   getUrlOptionsPart(): string {
@@ -387,7 +387,7 @@ export default class Nipp extends Vue {
     this.setLocationHash();
     // Transpile
     this.transpile();
-  };
+  }
 
   transpile() {
     try {
@@ -468,7 +468,7 @@ export default class Nipp extends Vue {
     // (from: http://d.hatena.ne.jp/osyo-manga/20140717/1405626111)
     const url = 'https://twitter.com/share?text='+encodeURIComponent(this.pageTitle)+"&url=" + encodeURIComponent(location.href)+"&hashtags=nipp";
     window.open(url,'','scrollbars=yes,width=500,height=300,');
-  };
+  }
 }
 </script>
 
