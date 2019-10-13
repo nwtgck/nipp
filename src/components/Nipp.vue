acdd5afe2631b9fecc2aa848fcf6ed43537be98c
Ryo Ota <nwtgck@gmail.com>
2019-10-13 13:27:56 +0900

    [change] Import pako asynchronously

--- src/components/Nipp.vue
+++ src/components/Nipp.vue
@@ -61,7 +61,7 @@
 
 <script lang="ts">
 import {Component, Prop, Vue, Watch} from 'vue-property-decorator';
-import * as pako from 'pako';
+const pakoAsync = () => import('pako');
 import * as uaDeviceDetector from 'ua-device-detector';
 import * as monacoEditor from 'monaco-editor'
 const MonacoEditor = () => import('vue-monaco');
@@ -130,12 +130,14 @@ type CompressionAlg = {
 const DeflateAlg: CompressionAlg = {
   name: "Deflate",
   compress: async (str: string) => {
+    const pako = await pakoAsync();
     // NOTE: Negative windowBits means no header and no checksum
     // (see: https://docs.python.org/3.6/library/zlib.html#zlib.decompress)
     const binStr = pako.deflate(str, {to: 'string', level: 9, windowBits: -8});
     return binStr;
   },
   decompress: async (binStr: string) => {
+    const pako = await pakoAsync();
     // NOTE: Negative windowBits means no header and no checksum
     // (see: https://docs.python.org/3.6/library/zlib.html#zlib.decompress)
     return pako.inflate(binStr, {to: 'string', windowBits: -8});
