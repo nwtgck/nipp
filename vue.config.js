e593b9c882105a96e61dc099be524a7c672d9d55
Ryo Ota <nwtgck@nwtgck.org>
2023-01-23 19:32:21 +0900

    fix: PWA cache

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,12 @@ module.exports = defineConfig({
   transpileDependencies: true,
   // (from: https://cli.vuejs.org/config/#publicpath)
   publicPath: "./",
+  pwa: {
+    workboxOptions: {
+      // NOTE: The size of ts.worker.js in monaco-editor is over maximumFileSizeToCacheInBytes default value
+      maximumFileSizeToCacheInBytes: Number.MAX_SAFE_INTEGER,
+    },
+  },
   configureWebpack: {
     plugins: [
       // (from: https://github.com/microsoft/monaco-editor/blob/212670ceb460441b3ebed29e6ca30aa1e9bdde85/docs/integrate-esm.md#option-1-using-the-monaco-editor-webpack-plugin)
